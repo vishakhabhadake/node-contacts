b4344ec9d1459e50ffb103b36b65e004871bcda9
vbhadake <vishakha_bhadake@bmc.com>
2023-09-10 13:44:41 +0530

    connection to mongodb config

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnection");
 
 const dotenv = require("dotenv").config();
 
+connectDb();
 const app = express();
 
 // const port = process.env.PORT || 5000;
